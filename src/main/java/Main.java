e02c32356fb7c67ecbba4c385e6ce78154b02334
menego <menego1983@gmail.com>
2016-11-26 00:03:28 +0100

    Start json config file parsing.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,32 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Iterator;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        try {
+
+            File jsonConfig = new File("%PROGRAMDATA%/SteelSeries/SteelSeries Engine 3/coreProps.json");
+
+            InputStream is = new FileInputStream(jsonConfig);
+
+            JSONTokener tokener = new JSONTokener(is);
+
+            JSONObject jsonObject = new JSONObject(tokener);
+
+            
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
