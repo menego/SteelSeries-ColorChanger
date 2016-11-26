2cd02bdbda73220f4f46de82e144df96ad586a24
menego <menego1983@gmail.com>
2016-11-26 09:23:56 +0100

    Add controls to check operative system and configuration file (forgot to add 2 other modified files).

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,8 @@
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
-import java.util.Iterator;
 
-import org.json.JSONArray;
+import com.utilities.Utility;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
@@ -13,17 +12,25 @@ public class Main {
     public static void main(String[] args) {
 
         try {
+            if(Utility.isWindows()) {
+                File jsonConfig = new File(System.getenv("PROGRAMDATA") + "/SteelSeries/SteelSeries Engine 3/coreProps.json");
 
-            File jsonConfig = new File("%PROGRAMDATA%/SteelSeries/SteelSeries Engine 3/coreProps.json");
+                if (jsonConfig.exists()) {
 
-            InputStream is = new FileInputStream(jsonConfig);
+                    InputStream is = new FileInputStream(jsonConfig);
 
-            JSONTokener tokener = new JSONTokener(is);
+                    JSONTokener tokener = new JSONTokener(is);
 
-            JSONObject jsonObject = new JSONObject(tokener);
+                    JSONObject jsonObject = new JSONObject(tokener);
 
-            
-
+                    System.out.println(jsonObject);
+                } else {
+                    throw new Exception("SteelSeries configuration file not found.");
+                }
+            }
+            else {
+                throw new Exception("Operative System not supported");
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
